c5e21e7a0984569a1316a340490e1702011b312f
laban254 <labanrotich6544@gmail.com>
2024-07-04 21:48:46 +0300

    refresh token endpoint

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import UserRegistrationAPIView, UserLoginAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', UserRegistrationAPIView.as_view(), name='user-register'),
     path('login/', UserLoginAPIView.as_view(), name='user-login'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
