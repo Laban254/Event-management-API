4e4c963d1549c17eeb6737fd1ae2d3f3af60d20e
Kibe <64686919+Laban254@users.noreply.github.com>
2024-07-26 22:05:23 +0300

    Add tags  for openapi doc

--- src/events/views.py
+++ src/events/views.py
@@ -9,6 +9,7 @@ from .serializers import (
     ScenarioSerializer, AwardSerializer, AttachmentSerializer, 
     TemplateSerializer, EventRuleSerializer, EventLogSerializer
 )
+from drf_yasg.utils import swagger_auto_schema
 from .permissions import IsEventOwnerOrReadOnly
 from rest_framework.parsers import MultiPartParser
 from .schema_extensions import (
@@ -20,6 +21,7 @@ from .schema_extensions import (
 
 @event_viewset_schema
 class EventViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Event'])
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticated, IsEventOwnerOrReadOnly]
@@ -91,46 +93,55 @@ class EventViewSet(viewsets.ModelViewSet):
 
 @item_viewset_schema
 class ItemViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Item'])
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 @participant_viewset_schema
 class ParticipantViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Participant'])
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
 
 @bid_viewset_schema
 class BidViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Bid'])
     queryset = Bid.objects.all()
     serializer_class = BidSerializer
 
 @scenario_viewset_schema
 class ScenarioViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Scenario'])
     queryset = Scenario.objects.all()
     serializer_class = ScenarioSerializer
 
 @award_viewset_schema
 class AwardViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Award'])
     queryset = Award.objects.all()
     serializer_class = AwardSerializer
 
 @attachment_viewset_schema
 class AttachmentViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Attachment'])
     queryset = Attachment.objects.all()
     serializer_class = AttachmentSerializer
     parser_classes = [MultiPartParser]
 
 @template_viewset_schema
 class TemplateViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['Template'])
     queryset = Template.objects.all()
     serializer_class = TemplateSerializer
 
 @event_rule_viewset_schema
 class EventRuleViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['EventRule'])
     queryset = EventRule.objects.all()
     serializer_class = EventRuleSerializer
 
 @event_log_viewset_schema
 class EventLogViewSet(viewsets.ModelViewSet):
+    @swagger_auto_schema(tags=['EventLog'])
     queryset = EventLog.objects.all()
     serializer_class = EventLogSerializer
